b3c039de9d182bf96180cb5a9307d8b1fb40f648
ruzzty06 <ruzzty06@yahoo.com>
2017-05-08 00:52:36 +0100

    Initial Upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static bool isLeap { get; set; }
+        enum Days { Sun=1, Mon, Tue, Wed, Thu, Fri, Sat };
+        
+        static void Main(string[] args)
+        {
+           // int dayOfWeek = 0;
+            int year = 2014;
+            
+            List<string> months = new List<string> {"January","February","March","April","May","June","July","August","September","October","November","December" };
+            List<string> weeks = new List<string> {"Sun","Mon","Tue","Wed","Thu","Fri","Sat" };
+            Console.Clear();
+            bool validInput=false;
+            while (!validInput)
+            {
+                Console.Write("ENTER YEAR:");
+                string inpt=Console.ReadLine();
+                if(int.TryParse(inpt,out year))
+                {
+                    if(year>0&year<9999)
+                        validInput=true;
+                }
+                
+                
+            }
+            Console.WriteLine("\nCALENDAR "+year+"\n");
+            Days dayStart = GetDayofYear(year);
+            foreach (string m in months)
+            {
+                Console.WriteLine(m + " " + year);
+                int Tdays = GetNumberofDays(m);
+                int day = 1;
+                for (int x = 1; x <= 7; x++)
+                {
+                    Console.Write((Days)x+"\t");
+                    if (x == 7)
+                        Console.WriteLine();
+                }
+
+                Days dayPlot = dayStart;
+                for (int x = 1; x < (int)dayStart;x++ )
+                {
+                    Console.Write("\t");
+                }
+                while(day<Tdays)
+                {
+                    while (dayPlot <(Days) 8)
+                    {
+                        if (day <= Tdays)
+                        {
+                            Console.Write(day.ToString("00") + "\t");
+                            day++;
+                            if (dayPlot == (Days)7)
+                            {
+                                Console.WriteLine();
+                                dayPlot = (Days)1;
+                            }
+                            else
+                                dayPlot++;
+                        }
+                        else
+                            break;
+                        
+                    }
+                    
+                    
+                }
+                dayStart = dayPlot;
+                Console.WriteLine(); 
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            
+        }
+        static int GetNumberofDays(string month)
+        {
+            switch (month)
+            {
+                case "January":
+                case "March":
+                case "May":
+                case "July":
+                case "August":
+                case "October":
+                case "December":
+                    return 31;
+                case "February":
+                    if(isLeap)
+                        return 29;
+                    return 28;
+                case "April":
+                case "June":
+                case "September":
+                case "November":
+                    return 30;
+                default:
+                    throw new Exception("Unexpected error. Month not found.");
+
+            }
+        }
+        static Days GetDayofYear(int year)
+        {
+            int tmpYear=2017;
+            Days BaseDay = Days.Sun;
+            
+            if (year <= tmpYear)
+            {
+                while (tmpYear > year)
+                {
+                    isLeap = false;
+                    tmpYear--;
+                    DecreaseDay(ref BaseDay);
+                    if (checkIfLeap(tmpYear))
+                    {
+                        DecreaseDay(ref BaseDay);
+                        isLeap = true;
+                    }
+                    
+                }
+            }
+            if (year > tmpYear)
+            {
+                
+                while (tmpYear < year)
+                {
+                    isLeap = false;
+                    tmpYear++;
+                    IncreaseDay(ref BaseDay);
+                    if (checkIfLeap(tmpYear-1))
+                    {
+                        IncreaseDay(ref BaseDay);
+                    }
+                    if (checkIfLeap(tmpYear))
+                        isLeap = true;
+
+                    
+                }
+            }
+            return BaseDay;
+        }
+        static void DecreaseDay(ref Days day)
+        {
+            if (day == (Days)1)
+                day = (Days)7;
+            else
+                day--;
+        }
+        static void IncreaseDay(ref Days day)
+        {
+            if (day == (Days)7)
+                day = (Days)1;
+            else
+                day++;
+        }
+        static bool checkIfLeap(int year)
+        {
+            if ( year%4 == 0)
+            {
+                if (year % 100 != 0)
+                    return true;
+                else if (year % 400==0)
+                    return true;
+            }
+            return false;
+                
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cntstCnt = 0;
+            bool validInput=false;
+            List<int> contestants = new List<int>();
+            //get number of contestants
+            while (!(validInput))
+            {
+                Console.Write("Enter the number of Contestant:");
+                string inpt = Console.ReadLine();
+                if (int.TryParse(inpt, out cntstCnt))
+                {
+                    if (cntstCnt >=2)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Invalid number of Contestants.");
+            }
+            //get value of each contestant
+            for (int x = 1; x <= cntstCnt; x++)
+            {
+                validInput = false;
+                while (!(validInput))
+                {
+                    int tmp = 0;
+                    Console.Write("Enter Contestant # "+x+":");
+                    string inpt = Console.ReadLine();
+                    if (int.TryParse(inpt, out tmp))
+                    {
+                        contestants.Add(tmp);
+                    }
+                    Console.WriteLine("Invalid value. Must be positive/negative integer.");
+                }
+            }
+            bool done=false;
+            int rounds = 0;
+            do
+            {
+                rounds++;
+
+
+            }
+            while (!(done));
+            
+
+        }
+        static List<int> doRound(List<int> contestants)
+        {
+            int matches = 0;
+            if (contestants.Count % 2 == 0)
+            {
+                matches = contestants.Count / 2;
+            }
+            else
+                matches = (contestants.Count / 2)+1;
+                for (int x = 0; x <= matches; x++)
+                {
+                    //Default
+                    if(matches>(contestants.Count/2))
+                        Console.WriteLine(
+                    int winner=getWinner(
+                }
+        }
+        static int getWinner(int A, int B)
+        {
+
+        }
+    }
+}
